b9430d9dded672414333815a5035e1df0480516f
tranquockha12022003@gmail.com <tranquockha12022003@gmail.com>
2024-03-22 02:54:36 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,16 +1,46 @@
+from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.server
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    self.init_components(**properties)
-    anvil.server.connect("your_anvil_server_key")
+    def __init__(self, **properties):
+       
+        self.init_components(**properties)
+      
+    
 
-  def btnSort_click(self, **event_args):
-    numbers = self.txtNumbers.text
-    algorithm = self.dropdownAlgorithm.selected_value
-    sorted_numbers = anvil.server.call('sort_numbers', numbers, algorithm)
-    self.lblSortedNumbers.text = ', '.join(map(str, sorted_numbers))
+ 
+    def insertion_sort(self, arr):
+        # Thuật toán sắp xếp chèn
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
 
-anvil.server.connect("your_anvil_server_key")
-anvil.server.wait_forever()
+    def insertion_sort_Descending(self, arr):
+       
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and key > arr[j]:  
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
+
+    def btnSort(self, **event_args):
+
+          numbers = [int(num) for num in self.text_box_1.text.split()]
+        
+        
+          sorted_numbers = self.insertion_sort(numbers)
+        
+        
+          self.text_box_2.text = ' '.join(map(str, sorted_numbers))
+  
+          sorted_numbers_3 = self.insertion_sort_Descending(numbers)
+          self.text_box_3.text = ' '.join(map(str, sorted_numbers_3))
+      
